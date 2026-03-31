33c47f056a02412337d4450bfa11b2dc8c7c441f
Arsh <arshtoor1476@gmail.com>
2026-03-31 14:17:36 +1100

    fix

--- packages/square_turtle_pkg/src/square_turtle.py
+++ packages/square_turtle_pkg/src/square_turtle.py
@@ -1,42 +1,59 @@
 #!/usr/bin/env python3
 
-# Import Dependencies
-import rospy 
-from geometry_msgs.msg import Twist 
-import time 
+import rospy
+from geometry_msgs.msg import Twist
 
-def move_turtle_square(): 
+def move_turtle_square():
     rospy.init_node('turtlesim_square_node', anonymous=True)
-    
-    # Init publisher
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) 
-    rospy.loginfo("Turtles are great at drawing squares!")
 
-    ########## YOUR CODE GOES HERE ##########
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(10)
+
+    vel = Twist()
+
+    rospy.loginfo("Drawing square...")
+
+    rospy.sleep(2)  # allow connection
+
     while not rospy.is_shutdown():
-        
-        rate = rospy.Rate(1)  # 1 Hz, adjust as needed
-        
-        # Create a Twist message for moving forward 
-        cmd_vel_msg = Twist() 
-        cmd_vel_msg.linear.x = 2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
 
-        rate.sleep()
+        for i in range(4):
 
-        # Create a Twist message for moving backward 
-        cmd_vel_msg = Twist()
-        cmd_vel_msg.linear.x = -2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
+            # 🔹 Move forward
+            vel.linear.x = 2.0
+            vel.angular.z = 0.0
 
-        rate.sleep()
+            start = rospy.Time.now().to_sec()
+            while rospy.Time.now().to_sec() - start < 2:
+                pub.publish(vel)
+                rate.sleep()
 
-        ###########################################
+            # 🔹 Stop
+            vel.linear.x = 0.0
+            pub.publish(vel)
+            rospy.sleep(1)
 
-if __name__ == '__main__': 
+            # 🔹 Turn
+            vel.angular.z = 1.57
 
-    try: 
-        move_turtle_square() 
-    except rospy.ROSInterruptException: 
+            start = rospy.Time.now().to_sec()
+            while rospy.Time.now().to_sec() - start < 1:
+                pub.publish(vel)
+                rate.sleep()
+
+            # 🔹 Stop
+            vel.angular.z = 0.0
+            pub.publish(vel)
+            rospy.sleep(1)
+
+        rospy.loginfo("Square complete!")
+        break   # remove this if you want continuous squares
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        move_turtle_square()
+    except rospy.ROSInterruptException:
         pass
-        
