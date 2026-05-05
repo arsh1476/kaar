aba90181b8659e2ff68f81d08f08edbffd4d2fcd
Arsh <arshtoor1476@gmail.com>
2026-05-05 15:34:10 +1000

    changed

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -12,8 +12,8 @@ class Drive_Square:
         rospy.init_node('drive_square_node', anonymous=True)
 
        
-        self.pub = rospy.Publisher('/mybota0024/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/mybota0024/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
+        self.pub = rospy.Publisher('/mybota002410/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/mybota002410/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
 
         self.running = False   # prevent multiple triggers
 
